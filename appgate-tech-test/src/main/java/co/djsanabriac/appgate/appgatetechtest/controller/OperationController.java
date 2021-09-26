bb3e7138fad098fde555ab289e2366e02a4fbe62
Johan Sanabria <djsanabriac@unal.edu.co>
2021-09-25 20:47:52 -0500

    Random session id generation
    Postman collection and enviroment

--- appgate-tech-test/src/main/java/co/djsanabriac/appgate/appgatetechtest/controller/OperationController.java
+++ appgate-tech-test/src/main/java/co/djsanabriac/appgate/appgatetechtest/controller/OperationController.java
@@ -1,15 +1,35 @@
 package co.djsanabriac.appgate.appgatetechtest.controller;
 
+import co.djsanabriac.appgate.appgatetechtest.model.dto.GeneralResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @RestController
 public class OperationController {
 
+    Logger logger = LoggerFactory.getLogger(OperationController.class);
+
+    List<String> sessionList = new ArrayList<>();
+
     @GetMapping("/operation/session")
-    ResponseEntity<String> getSession(){
-        return ResponseEntity.ok("Session is working");
+    ResponseEntity getSession(){
+
+        String uuid = null;
+        do {
+            uuid = UUID.randomUUID().toString();
+        }while ( sessionList.contains(uuid) );
+
+        sessionList.add(uuid);
+        logger.info(sessionList.toString());
+
+        return ResponseEntity.ok(new GeneralResponse<>(true, "OK", uuid).toMap());
     }
 
 }
---
+++ appgate-tech-test/src/main/java/co/djsanabriac/appgate/appgatetechtest/model/dto/GeneralResponse.java
@@ -0,0 +1,64 @@
+package co.djsanabriac.appgate.appgatetechtest.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GeneralResponse<T> {
+
+    private Boolean success;
+    private String message;
+    private T data;
+
+    public GeneralResponse() {
+    }
+
+    public GeneralResponse(Boolean success,
+                           String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public GeneralResponse(Boolean success,
+                           String message,
+                           T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> toReturn = new HashMap<String, Object>();
+        toReturn.put("success", true);
+        toReturn.put("message", message);
+        toReturn.put("data", data);
+        return toReturn;
+    }
+
+    @Override
+    public String toString() {
+        return "GeneralResponse{" +
+                "success=" + success +
+                ", message='" + message + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
